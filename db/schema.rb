1c640a6bfb08a78a11f37e7d3b93882946e118e4
“MohamedYazarudeen” <mohamedyazarudeen1995@gmail.com>
2022-05-01 20:18:02 +0530

    Articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_29_154833) do
+ActiveRecord::Schema.define(version: 2022_05_01_143141) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "books", force: :cascade do |t|
